7d1f6f40246bce83033c8b87a7df2049c7f92923
sumanthreddy76 <sumanthreddynandyala@gmail.com>
2023-04-19 22:52:12 -0500

    bonus commit

--- controllers/glove.js
+++ controllers/glove.js
@@ -90,3 +90,76 @@ exports.glove_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+// code for Screnshot 4 and 5
+// Handle glove delete on DELETE.
+exports.glove_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await glove.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+
+   // code for SS6
+
+   // Handle a show one view with id specified by query
+exports.glove_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await glove.findById( req.query.id)
+    res.render('glovedetail',
+   { title: 'glove Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   //code for SS7
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.glove_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('glovecreate', { title: 'glove Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   //code for SS8
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.glove_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await glove.findById(req.query.id)
+    res.render('gloveupdate', { title: 'glove Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   exports.glove_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await glove.findById(req.query.id)
+    res.render('glovedelete', { title: 'glove Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/glove.js
+++ routes/glove.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-const glove_controlers= require('../controllers/glove');
+const glove_controllers= require('../controllers/glove');
 /* GET home page. */
-router.get('/', glove_controlers.glove_view_all_Page);
+router.get('/', glove_controllers.glove_view_all_Page);
+
+//SS6
+/* GET detail glove page */
+router.get('/detail', glove_controllers.glove_view_one_Page);
+
+//SS7
+/* GET create glove page */
+router.get('/create', glove_controllers.glove_create_Page);
+
+
+//ss8
+/* GET create update page */
+router.get('/update', glove_controllers.glove_update_Page);
+
+router.get('/delete', glove_controllers.glove_delete_Page);
+
 module.exports = router;
